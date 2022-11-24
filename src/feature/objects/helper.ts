d0468409c81a7997229c6ff5837a0d3952d4a889
pvignau <vignau.py@gmail.com>
2022-11-24 23:30:34 +0100

    feat(collisions): update hero move helper

--- src/feature/objects/helper.ts
+++ src/feature/objects/helper.ts
@@ -1,45 +1,62 @@
-import { IHero } from "../../types"
-
 import { store } from "../../store"
 import { doObjectsCollide } from "./collisionHelper";
 
 const ObjectMoveHelper = {
-    
-    canMoveUp (object: IHero, speed: number) {
-        let canMove = true;
-        store.getState().map.collisionTiles.forEach((tile: {x: number, y: number }) => {
-            if (doObjectsCollide({...store.getState().hero.hero.position, y: store.getState().hero.hero.position.y - speed}, tile)) {
-              canMove = false;
-            }
-          })
-        return canMove;
+    getTiles () {
+      return [...store.getState().map.collisionTiles, ...store.getState().map.metaTiles]
     },
-    canMoveDown (object: IHero, speed: number) {
-        let canMove = true;
-        store.getState().map.collisionTiles.forEach((tile: {x: number, y: number }) => {
-            if (doObjectsCollide({...store.getState().hero.hero.position, y: store.getState().hero.hero.position.y + speed}, tile)) {
-              canMove = false;
-            }
-          })
-        return canMove;
+    getHeroFuturePosition (direction: string,speed: number) {
+      switch (direction) {
+        case 'up':
+          return {...store.getState().hero.hero.position, y: store.getState().hero.hero.position.y - speed}
+        case 'down':
+          return {...store.getState().hero.hero.position, y: store.getState().hero.hero.position.y + speed}
+        case 'left':
+          return {...store.getState().hero.hero.position, x: store.getState().hero.hero.position.x - speed};
+        case 'right':
+          return {...store.getState().hero.hero.position, x: store.getState().hero.hero.position.x + speed}
+      }
+      return store.getState().hero.hero.position
     },
-    canMoveLeft (object: IHero, speed: number) {
-        let canMove = true;
-        store.getState().map.collisionTiles.forEach((tile: {x: number, y: number }) => {
-            if (doObjectsCollide({...store.getState().hero.hero.position, x: store.getState().hero.hero.position.x - speed}, tile)) {
-              canMove = false;
-            }
-          })
-        return canMove;
+    canMoveUp (speed: number) {
+        const tiles = this.getTiles();
+        for(let i = 0; i <tiles.length; i++) {
+          if (doObjectsCollide(this.getHeroFuturePosition('up', speed), tiles[i])) {
+            console.log(tiles[i]);
+            return tiles[i];
+          }
+        }
+        return true;
     },
-    canMoveRight (object: IHero, speed: number) {
-        let canMove = true;
-        store.getState().map.collisionTiles.forEach((tile: {x: number, y: number }) => {
-            if (doObjectsCollide({...store.getState().hero.hero.position, x: store.getState().hero.hero.position.x + speed}, tile)) {
-              canMove = false;
-            }
-          })
-        return canMove;
+    canMoveDown (speed: number) {
+      const tiles = this.getTiles();
+        for(let i = 0; i <tiles.length; i++) {
+          if (doObjectsCollide(this.getHeroFuturePosition('down', speed), tiles[i])) {
+            console.log(tiles[i]);
+            return tiles[i];
+          }
+        }
+        return true;
+    },
+    canMoveLeft (speed: number) {
+      const tiles = this.getTiles();
+        for(let i = 0; i <tiles.length; i++) {
+          if (doObjectsCollide(this.getHeroFuturePosition('left', speed), tiles[i])) {
+            console.log(tiles[i]);
+            return tiles[i];
+          }
+        }
+        return true;
+    },
+    canMoveRight (speed: number) {
+      const tiles = this.getTiles();
+        for(let i = 0; i <tiles.length; i++) {
+          if (doObjectsCollide(this.getHeroFuturePosition('right', speed), tiles[i])) {
+            console.log(tiles[i]);
+            return tiles[i];
+          }
+        }
+        return true;
     },
 }
 
