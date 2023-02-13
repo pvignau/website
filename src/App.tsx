2210e0de153b8c9261887dbc01f8ffe565ae24a4
pvignau <vignau.py@gmail.com>
2023-02-14 00:20:30 +0100

    feat(map): init map on main component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,21 @@
 import './App.scss';
 import MapEngine from './components/MapEngine';
 import Loader from './components/Loader';
+//import WorldMap from './maps/world.png'
+import WorldJson from './maps/world.json'
 import type { RootState } from './store';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import useMove from './hooks/useMove';
+import { initMap } from './store/slices/mapReducer';
+import { useEffect } from 'react';
 
 function App() {
   const isLoading = useSelector((state: RootState) => state.root.isLoading); // This makes a ref event if we do not use it ? Black magic ... 
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(initMap({name: 'world', json: WorldJson }));
+  })
 
   useMove();
 
