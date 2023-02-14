58048510cccb44d9be0e7ce25db2fa00ca0fc4f6
PIerre-Yves Vignau <vignau.py@gmail.com>
2023-02-14 10:06:45 +0100

    Update src/store/slices/mapReducer.ts

--- src/store/slices/mapReducer.ts
+++ src/store/slices/mapReducer.ts
@@ -14,7 +14,7 @@ const layerToTiles = function (layer: any): ITile[] {
   while(layer.data.length > 0) {
     rawTiles.push(layer.data.splice(0, layer.width));
   }
-  let height = (layer.name === 'collisions') ? 12 : 0, // Specific to fix collisions case
+  let height = (layer.name === 'collisions') ? 12 : 0, // FIXME: Specific to fix collisions case
     width = 0;
     const tileMeta = {
       action: (layer.properties[0].name === 'address') ? 'redirect' : 'collide',
